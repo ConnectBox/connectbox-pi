ebf370b7f543912a53efad1dbe3566d65ed04b64
Edwin Steele <edwin-lists@wordspeak.org>
2016-11-14 16:36:43 +1100

    Stop using pushd and popd and just use cd

--- ci/script_run_on_non_pull_requests.sh
+++ ci/script_run_on_non_pull_requests.sh
@@ -7,13 +7,11 @@
 #  directory left for subsequent scripts
 
 # Extract Encrypted ssh key
-pushd $TRAVIS_BUILD_DIR;
+cd $TRAVIS_BUILD_DIR;
 openssl aes-256-cbc -K $encrypted_22a22c63eb0e_key -iv $encrypted_22a22c63eb0e_iv -in ci/travis-ci-biblebox.pem.enc -out ci/travis-ci-biblebox.pem -d;
-popd;
 
 # Run CI build on AWS. This uses protected variables
-pushd $TRAVIS_BUILD_DIR/ci;
+cd $TRAVIS_BUILD_DIR/ci;
 vagrant up --no-provision;
-popd;
 
 
