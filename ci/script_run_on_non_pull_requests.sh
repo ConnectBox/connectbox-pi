861e70d822726940ebe984c77fb884add5c52f40
Edwin Steele <edwin-lists@wordspeak.org>
2016-11-15 17:34:40 +1100

    More troubleshooting

--- ci/script_run_on_non_pull_requests.sh
+++ ci/script_run_on_non_pull_requests.sh
@@ -13,19 +13,12 @@ touch $PEM_OUT;
 chmod 600 $PEM_OUT;
 openssl aes-256-cbc -K $encrypted_22a22c63eb0e_key -iv $encrypted_22a22c63eb0e_iv -in ci/travis-ci-biblebox.pem.enc -d >> $PEM_OUT;
 
-ls -l ci;
-md5sum ci/*.pem;
-cat /home/travis/build/edwinsteele/biblebox-pi/ci/.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
-echo "AWS_ACCESS_KEY_ID"
-echo $AWS_ACCESS_KEY_ID | md5sum
-echo "AWS_SECRET_ACCESS_KEY"
-echo $AWS_SECRET_ACCESS_KEY | md5sum
 
 # Run CI build on AWS. This uses protected variables
 cd $TRAVIS_BUILD_DIR/ci;
 vagrant up;
 
-find $TRAVIS_BUILD_DIR/ci/.vagrant
+cat /home/travis/build/edwinsteele/biblebox-pi/ci/.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
 
 ansible-playbook -vvvv -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../ansible/site.yml
 
