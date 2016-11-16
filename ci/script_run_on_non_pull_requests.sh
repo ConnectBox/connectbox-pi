0933b3401e92688d9b8c7572535c291f19815131
Edwin Steele <edwin-lists@wordspeak.org>
2016-11-16 17:36:51 +1100

    Perform CI of ansible playbooks in AWS on Debian Jessie (#10)
    * Spawn an instance in AWS with the current Debian Jessie and run playbooks against it for each merge to master, or commit on master.
    * Add aptitude package to Raspian simulation layer as the official Debian Jessie AMIs lack it (and it causes a playbook failure)

---
+++ ci/script_run_on_non_pull_requests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# This is only run for NON pull requests i.e. merges and comments per suggestion at:
+# https://docs.travis-ci.com/user/pull-requests#Pull-Requests-and-Security-Restrictions
+
+# Makes no assumptions about working directory on entry, or the working
+#  directory left for subsequent scripts
+
+# Extract Encrypted ssh key
+cd $TRAVIS_BUILD_DIR;
+PEM_OUT=$TRAVIS_BUILD_DIR/ci/travis-ci-biblebox.pem;
+touch $PEM_OUT;
+chmod 600 $PEM_OUT;
+openssl aes-256-cbc -K $encrypted_22a22c63eb0e_key -iv $encrypted_22a22c63eb0e_iv -in ci/travis-ci-biblebox.pem.enc -d >> $PEM_OUT;
+
+# Run CI build on AWS. This uses protected variables
+cd $TRAVIS_BUILD_DIR/ci;
+vagrant up;
+
+# Perform a re-run of the playbooks, to see whether they run cleanly and
+#  without marking any task as changed
+# vagrant provision
