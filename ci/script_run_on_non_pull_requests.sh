b1cc847284049757f6060eb156d1f32904cfc3fe
Edwin Steele <edwin-lists@wordspeak.org>
2016-11-16 17:08:31 +1100

    Reinstate instance destroy. Remove debugging statements

--- ci/script_run_on_non_pull_requests.sh
+++ ci/script_run_on_non_pull_requests.sh
@@ -13,20 +13,10 @@ touch $PEM_OUT;
 chmod 600 $PEM_OUT;
 openssl aes-256-cbc -K $encrypted_22a22c63eb0e_key -iv $encrypted_22a22c63eb0e_iv -in ci/travis-ci-biblebox.pem.enc -d >> $PEM_OUT;
 
-
 # Run CI build on AWS. This uses protected variables
 cd $TRAVIS_BUILD_DIR/ci;
 vagrant up;
 
-ansible-playbook -vvvv -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../ansible/site.yml
-
-vagrant_inv=/home/travis/build/edwinsteele/biblebox-pi/ci/.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory
-cat $vagrant_inv
-
-aws_host=$(cut -d" " -f2 $vagrant_inv | cut -d"=" -f2)
-echo "aws host"
-echo $aws_host
-
-ssh -C -vvv -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o BatchMode=yes -o Port=22 -o 'IdentityFile="/home/travis/build/edwinsteele/biblebox-pi/ci/travis-ci-biblebox.pem"' -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o User=admin -o ConnectTimeout=10 -o ControlPath=/home/travis/.ansible/cp/%C $aws_host "hostname"
-
-ssh -C -vvv -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o BatchMode=yes -o Port=22 -i $PEM_OUT -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o User=admin -o ConnectTimeout=10 -o ControlPath=/home/travis/.ansible/cp/%C $aws_host "hostname"
+# Perform a re-run of the playbooks, to see whether they run cleanly and
+#  without marking any task as changed
+# vagrant provision
