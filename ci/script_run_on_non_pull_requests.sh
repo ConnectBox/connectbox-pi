993f02e19f01376ee16460eb0547262557c6633e
Edwin Steele <edwin-lists@wordspeak.org>
2016-11-26 09:46:34 +1100

    Work around bad behaviour in synchronise task when the ansible_ssh_host is not the same as the name given in the first column of hte inventory

--- ci/script_run_on_non_pull_requests.sh
+++ ci/script_run_on_non_pull_requests.sh
@@ -15,7 +15,22 @@ openssl aes-256-cbc -K $encrypted_22a22c63eb0e_key -iv $encrypted_22a22c63eb0e_i
 
 # Run CI build on AWS. This uses protected variables
 cd $TRAVIS_BUILD_DIR/ci;
-vagrant up;
+vagrant up --no-provision;
+
+# We need to assemble an inventory file ourselves. If we use the one that
+#  vagrant automatically creates, it has the hostname as "default" which
+#  confuses the ansible synchronize task. The synchronize task has some
+#  logic that determines whether the rsync arguments involve a remote host
+#  and because "default" doesn't match the machine name, it thinks that
+#  the remote endpoint requires an ssh connection, and because keys aren't
+#  accessible, the connection fails.
+# So we assemble an inventory file ourselves.
+target_host=$(vagrant ssh-config | awk '$1 ~ /HostName/ { print $2; }')
+ssh_user=$(vagrant ssh-config | awk '$1 ~ /User/ { print $2; }')
+echo "$target_host ansible_ssh_user=$ssh_user ansible_ssh_private_key_file=$PEM_OUT" > $INVENTORY_FILE;
+
+# Now do our initial provisioning run
+vagrant provision
 
 # Perform a re-run of the playbooks, to see whether they run cleanly and
 #  without marking any task as changed
