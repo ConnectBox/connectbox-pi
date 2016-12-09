cabb4f9d6304df9efe6d1677b4ba9de240dc4a80
Edwin Steele <edwin-lists@wordspeak.org>
2016-12-10 06:26:23 +1100

    Get test data

--- ci/script_run_on_non_pull_requests.sh
+++ ci/script_run_on_non_pull_requests.sh
@@ -16,7 +16,22 @@ openssl aes-256-cbc -K $encrypted_22a22c63eb0e_key -iv $encrypted_22a22c63eb0e_i
 # Run CI build on AWS. This uses protected variables
 cd $TRAVIS_BUILD_DIR/ci;
 
+date
 terraform apply;
+date
+
+ping -c 5 dynamodb.us-east-1.amazonaws.com
+ping -c 5 dynamodb.us-west-1.amazonaws.com
+ping -c 5 dynamodb.us-west-2.amazonaws.com
+ping -c 5 dynamodb.eu-west-1.amazonaws.com
+ping -c 5 dynamodb.eu-central-1.amazonaws.com
+ping -c 5 dynamodb.ap-south-1.amazonaws.com
+ping -c 5 dynamodb.ap-northeast-2.amazonaws.com
+ping -c 5 dynamodb.ap-southeast-1.amazonaws.com
+ping -c 5 dynamodb.ap-southeast-2.amazonaws.com
+ping -c 5 dynamodb.ap-northeast-1.amazonaws.com
+ping -c 5 dynamodb.sa-east-1.amazonaws.com
+ping -c 5 dynamodb.cn-north-1.amazonaws.com.cn
 
 target_host=$(terraform output biblebox-server-public-ip);
 
@@ -25,16 +40,25 @@ echo "${target_host} ansible_ssh_user=admin ansible_ssh_private_key_file=$PEM_OU
 cat inventory;
 
 # Wait for ssh to become available
-sleep 90;
+while ! (ssh -o ConnectTimeout=2 -i ${PEM_OUT} admin@${target_host} /usr/bin/true 2> /dev/null); do
+  echo -n ".";
+done
+echo "OK";
 
 # Now do our initial provisioning run
+date
 ansible-playbook -i inventory ../ansible/site.yml;
+date
 
 # Perform a re-run of the playbooks, to see whether they run cleanly and
 #  without marking any task as changed
+date
 ansible-playbook -i inventory ../ansible/site.yml;
+date
 
 # Tell the test running host how to find the biblebox by name
 echo "\n${target_host} biblebox.local" | sudo tee -a /etc/hosts > /dev/null
 # Run web/selenium tests
+date
 TEST_IP=$target_host python -m unittest discover ../tests
+date
