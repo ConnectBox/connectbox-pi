b2dd5b1af75564a37e3a5f8c4fb116dad08a8691
Edwin Steele <edwin-lists@wordspeak.org>
2016-12-26 08:41:12 +1100

    Write tests for SSID API. Fix bug in mgmt script
    - Fix ssid management script, that was associating a zero exit code with
      failure instead of success
    - Create wlan0 interface when on debian to allow restarting of wlan0
      which is necessary for testing
    - Add SSID round-trip tests and input validation tests

--- tests/test_connectbox_static.py
+++ tests/test_connectbox_static.py
@@ -1,5 +1,7 @@
+import json
 import os
 import unittest
+import random
 import requests
 from selenium import webdriver
 
@@ -45,12 +47,71 @@ class ConnectBoxBasicTestCase(unittest.TestCase):
         r = requests.get("%s/" % (ADMIN_BASE_URL,), auth=getAdminAuth())
         self.assertIn("ConnectBox Admin Dashboard", r.text)
 
+
+class ConnectBoxAPITestCase(unittest.TestCase):
+
+    ADMIN_SSID_URL = "%s/api.php/ssid" % (ADMIN_BASE_URL,)
+    ADMIN_HOSTNAME_URL = "%s/api.php/hostname" % (ADMIN_BASE_URL,)
+    SUCCESS_RESPONSE = ["SUCCESS"]
+    BAD_REQUEST_TEXT = "BAD REQUEST"
+
     def testAdminApiSmoketest(self):
         # To catch where there is a gross misconfiguration that breaks
         #  nginx/php
-        r = requests.get("%s/api.php/hostname" % (ADMIN_BASE_URL,),
-                         auth=getAdminAuth())
+        r = requests.get(self.ADMIN_HOSTNAME_URL, auth=getAdminAuth())
+        r.raise_for_status()
         self.assertEquals(r.json()["code"], 0)
+
+    def testSSIDUnchRoundTrip(self):
+        r = requests.get(self.ADMIN_SSID_URL, auth=getAdminAuth())
+        initial_ssid = r.json()["result"][0]
+        r = requests.put(self.ADMIN_SSID_URL, auth=getAdminAuth(),
+                         data=json.dumps({"value": initial_ssid}))
+        r.raise_for_status()
+        self.assertEquals(self.SUCCESS_RESPONSE, r.json()["result"])
+        r = requests.get(self.ADMIN_SSID_URL, auth=getAdminAuth())
+        r.raise_for_status()
+        final_ssid = r.json()["result"][0]
+        self.assertEquals(initial_ssid, final_ssid)
+
+    def testSetSSID(self):
+        new_ssid = "ssid-%s" % (random.randint(0, 1000000000),)
+        r = requests.put(self.ADMIN_SSID_URL, auth=getAdminAuth(),
+                         data=json.dumps({"value": new_ssid}))
+        r.raise_for_status()
+        self.assertEquals(self.SUCCESS_RESPONSE, r.json()["result"])
+        r = requests.get(self.ADMIN_SSID_URL, auth=getAdminAuth())
+        self.assertEquals(new_ssid, r.json()["result"][0])
+
+    def testBadRequestOnIncorrectRequestType(self):
+        # Need to use PUT not POST
+        r = requests.post(self.ADMIN_SSID_URL, auth=getAdminAuth(),
+                          data=json.dumps({"value": "some_ssid"}))
+        with self.assertRaises(requests.HTTPError) as cm:
+            r.raise_for_status()
+
+        self.assertEqual(cm.exception.response.status_code, 400)
+        self.assertEqual(cm.exception.response.text, self.BAD_REQUEST_TEXT)
+
+    def testBadRequestOnIncorrectDataType(self):
+        # Need to use JSON encoded params
+        r = requests.put(self.ADMIN_SSID_URL, auth=getAdminAuth(),
+                         data="value=some_ssid")
+        with self.assertRaises(requests.HTTPError) as cm:
+            r.raise_for_status()
+
+        self.assertEqual(cm.exception.response.status_code, 400)
+        self.assertEqual(cm.exception.response.text, self.BAD_REQUEST_TEXT)
+
+    def testBadRequestOnIncorrectFormVariable(self):
+        # Need to use 'value' not 'ssid'
+        r = requests.put(self.ADMIN_SSID_URL, auth=getAdminAuth(),
+                         data=json.dumps({"ssid": "some_ssid"}))
+        with self.assertRaises(requests.HTTPError) as cm:
+            r.raise_for_status()
+
+        self.assertEqual(cm.exception.response.status_code, 400)
+        self.assertEqual(cm.exception.response.text, self.BAD_REQUEST_TEXT)
 
 
 class ConnectBoxWebDriverTestCase(unittest.TestCase):
