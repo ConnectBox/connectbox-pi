d69014a15756df48b73848b5d2b1a25a6582cb88
Edwin Steele <edwin@wordspeak.org>
2017-01-10 17:45:34 +1100

    Retry on terraform connectivity probs (#57)
    * Have a second go at running terraform apply if we can't establish ssh connections.
    * Make sure we're always destroying infrastructure on failed connections (this wasn't the case when builds hit their 49 minute timeout, as travis doesn't run build end hooks when the build is killed

--- ci/script_run_on_non_pull_requests.sh
+++ ci/script_run_on_non_pull_requests.sh
@@ -6,6 +6,47 @@
 # Makes no assumptions about working directory on entry, or the working
 #  directory left for subsequent scripts
 
+# Terraform output variables that define endpoints we'll sshe to
+PROVISIONED_TARGET_IP_VARIABLES="connectbox-server-public-ip";
+
+# Servers should take 3 minutes max to become accessible over ssh, and as
+#  we wait for 1 second between attempts, this is our max attempt count
+MAX_SSH_CONNECT_ATTEMPTS=180;
+
+setup_and_verify_infra( ) {
+  # Expects arg of PROVISIONED_TARGET_IP_VARIABLES
+  # Make sure we don't have an inventory file, as we're going to append to it
+  #  as we run this function.
+  rm -f inventory;
+  terraform apply;
+
+  for target_host_var in $1; do
+    conn_attempt_count=0;
+    target_host=$(terraform output $target_host_var);
+
+    # Wait for ssh to become available on the target host
+    echo -n "Waiting for ssh to become available on $target_host "
+    while ! (ssh -o ConnectTimeout=2 -o StrictHostKeyChecking=no -i $PEM_OUT admin@$target_host true 2> /dev/null); do
+      if [ $conn_attempt_count -ge $MAX_SSH_CONNECT_ATTEMPTS ]; then
+	# Something has gone wrong. Bail (don't even attempt to connect to
+	#  any other hosts provisioned in the same terraform apply).
+	echo " unable to connect in $conn_attempt_count attempts.";
+	return 1;
+      fi
+      echo -n ".";
+      conn_attempt_count=$(( $conn_attempt_count + 1 ));
+      sleep 1;
+    done
+    echo "OK";
+    echo "Adding $target_host to inventory";
+    echo "$target_host ansible_ssh_user=admin ansible_ssh_private_key_file=$PEM_OUT" > inventory;
+  done
+
+  echo "Inventory follows:";
+  cat inventory;
+  return 0;
+}
+
 # Extract Encrypted ssh key
 cd $TRAVIS_BUILD_DIR;
 PEM_OUT=$TRAVIS_BUILD_DIR/ci/travis-ci-connectbox.pem;
@@ -16,23 +57,19 @@ openssl aes-256-cbc -K $encrypted_22a22c63eb0e_key -iv $encrypted_22a22c63eb0e_i
 # Run CI build on AWS. This uses protected variables
 cd $TRAVIS_BUILD_DIR/ci;
 
-time terraform apply;
-
-target_host=$(terraform output connectbox-server-public-ip);
-
-# Create an inventory file suitable for ansible
-echo "${target_host} ansible_ssh_user=admin ansible_ssh_private_key_file=$PEM_OUT" > inventory;
-
-echo "Inventory follows:"
-cat inventory
-
-# Wait for ssh to become available
-echo -n "Waiting for ssh to become available "
-while ! (ssh -o ConnectTimeout=2 -o StrictHostKeyChecking=no -i ${PEM_OUT} admin@${target_host} true 2> /dev/null); do
-  echo -n ".";
-  sleep 1;
-done
-echo "OK";
+setup_and_verify_infra $PROVISIONED_TARGET_IP_VARIABLES;
+if [ $? -ne 0 ]; then
+  # Try again.
+  echo "Tearing down freshly provisioned infrastructure and trying again.";
+  terraform destroy --force;
+  setup_and_verify_infra $PROVISIONED_TARGET_IP_VARIABLES;
+  if [ $? -ne 0 ]; then
+    # Something is seriously wrong, and we should bail
+    echo "Unable to connect to AWS infrastructure after two attempts. Tearing down freshly provisioned infrastructure and bailing."
+    terraform destroy --force;
+    exit 1;
+  fi
+fi
 
 # For builds not triggered by a pull request $TRAVIS_BRANCH is the name
 #  of the branch currently being built
