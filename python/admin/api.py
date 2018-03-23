8919dce6b6e8fb6e8f8b672619455341cb2d9112
Portablejim <dcseee-github@yahoo.com.au>
2018-03-23 15:24:03 +1100

    Implement admin api in python

--- python/admin/api.py
+++ python/admin/api.py
@@ -1,27 +1,59 @@
-import os
+import os,subprocess
+from flask import Flask,request,abort,jsonify
 
-from flask import Flask,request,abort
+valid_properties = ["ssid", "channel", "hostname", "staticsite", "password", "system", "ui-config"]
+    
 
-def get_property(property):
-    #if not request.json:
-    #    abort(400) # bad request
-    return "get " + property
+def _call_command(extra_args):
+    cmd_args = ["sudo", "/usr/local/connectbox/bin/ConnectBoxManage.sh"]
+    called_cmd = subprocess.run(cmd_args + extra_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if called_cmd.returncode == 0:
+        return jsonify(code=0, result=called_cmd.stdout.decode("utf-8").strip())
+    else:
+        return jsonify(code=called_cmd.returncode, result=called_cmd.stderr.decode("utf-8").strip())
 
 
-def set_property(property):
-    #if not request.json:
-    #    abort(400) # bad request
-    return "set " + property
+def get_property(prop):
+    prop_string = prop
+    if prop_string not in valid_properties:
+        abort(405)
+    return _call_command(["get", prop_string])
+
+
+def set_property(prop):
+    prop_string = prop
+    if prop_string not in valid_properties:
+        abort(400) # bad request
+    if not request.json:
+        abort(400) # bad request
+    return _call_command(["set", prop_string])
+
+
+def set_system_property():
+    if (not request.json) or ("value" not in request.json):
+        abort(400) # bad request
+
+    if request.json["value"] in ["shutdown", "reboot"]:
+        # Safe as input is limited
+        subprocess.Popen("sleep 1; " + request.json["value"], shell=True)
+        return jsonify(code=0, result="SUCCESS")
+
+    return _call_command([request.json["value"]])
 
 
 def register(app):
     app.add_url_rule(
-        rule='/admin/api/<property>',
+        rule='/admin/api/<prop>',
         endpoint='get_property',
         methods=['GET'],
         view_func=get_property)
     app.add_url_rule(
-        rule='/admin/api/<property>',
+        rule='/admin/api/system',
+        endpoint='set_system_property',
+        methods=['POST'],
+        view_func=set_system_property)
+    app.add_url_rule(
+        rule='/admin/api/<prop>',
         endpoint='set_property',
-        methods=['PUT','POST'],
+        methods=['PUT'],
         view_func=set_property)
