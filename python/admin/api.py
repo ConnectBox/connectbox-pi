7f457519d72f308bd49d9eb3628cf99de69dd337
Portablejim <portablejim@jamesmckay.id.au>
2018-04-25 23:25:17 +1000

    Fix most test cases

--- python/admin/api.py
+++ python/admin/api.py
@@ -1,7 +1,10 @@
 import os,subprocess
-from flask import Flask,request,abort,jsonify
+from flask import Flask,request,abort,jsonify,make_response
 
 valid_properties = ["ssid", "channel", "hostname", "staticsite", "password", "system", "ui-config"]
+
+def _abort_bad_request():
+    abort(make_response("BAD REQUEST", 400))
     
 
 def _call_command(extra_args):
@@ -19,26 +22,33 @@ def _call_command(extra_args):
 def get_property(prop):
     prop_string = prop
     if prop_string not in valid_properties:
-        abort(405)
+        _abort_bad_request()
     return _call_command(["get", prop_string])
+
+
+def set_property_value_wrapped(prop):
+    prop_string = prop
+    if prop_string not in valid_properties:
+        _abort_bad_request() # bad request
+    possible_json = request.get_json(force=True, silent=True)
+    if (not possible_json) or ("value" not in possible_json):
+        _abort_bad_request() # bad request
+    return _call_command(["set", prop_string, possible_json["value"]])
 
 
 def set_property(prop):
     prop_string = prop
     if prop_string not in valid_properties:
-        abort(400) # bad request
+        _abort_bad_request() # bad request
     possible_json = request.get_json(force=True, silent=True)
     if not possible_json:
-        abort(400) # bad request
-    target_value = str(possible_json)
-    if "value" in possible_json:
-        target_value = possible_json["value"]
-    return _call_command(["set", prop_string, target_value])
+        _abort_bad_request() # bad request
+    return _call_command(["set", prop_string, possible_json])
 
 
 def set_system_property():
     if (not request.json) or ("value" not in request.json):
-        abort(400) # bad request
+        _abort_bad_request() # bad request
 
     if request.json["value"] in ["shutdown", "reboot"]:
         # Safe as input is limited
@@ -60,7 +70,12 @@ def register(app):
         methods=['POST'],
         view_func=set_system_property)
     app.add_url_rule(
-        rule='/admin/api/<prop>',
+        rule='/admin/api/ui-config',
         endpoint='set_property',
         methods=['PUT'],
         view_func=set_property)
+    app.add_url_rule(
+        rule='/admin/api/<prop>',
+        endpoint='set_property_value_wrapped',
+        methods=['PUT'],
+        view_func=set_property_value_wrapped)
