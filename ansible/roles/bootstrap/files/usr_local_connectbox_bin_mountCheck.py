7625136e4fbbdba028e5774a54a6bc15167a9c86
Kirk Wilson <kirk.wilson@globalsystems.design>
2021-10-25 14:21:26 -0600

    Corrected auto mount/dismount to allow for sdb1 as well Fixed reboot on os upgrade

--- ansible/roles/bootstrap/files/usr_local_connectbox_bin_mountCheck.py
+++ ansible/roles/bootstrap/files/usr_local_connectbox_bin_mountCheck.py
@@ -1,25 +1,43 @@
+#!/usr/bin/env Python3
+# -*- coding: utf-8 -*-
 # Loop to test for insertion of USB stick and to mount it as /media/usb0
 #  Also tests for removal of USB and responds with umount /media/usb0
 
 import os
 import time
 
-usbMounted = False
+usbMounteda = False
+usbMountedb = False
 
 def mountCheck():
-    global usbMounted
+    global usbMounteda
+    global usbMountedb
     b = os.popen('lsblk').read()
     if 'sda1' in b:
-        if usbMounted == True:
+        if usbMounteda == True:
             return
-        res = os.system("mount /dev/sda1 /media/usb0")
+        res = os.system("mount /dev/sda1 /media/usb0 -o iocharset=utf8")
         if res == 0:
-            usbMounted = True
+            usbMounteda = True
     else:
-        if (usbMounted == False):
+        if (usbMounteda == False):
             return
         os.system("umount /media/usb0")
-        usbMounted = False
+        usbMounteda = False
+
+    if 'sdb1' in b:
+        if usbMounteda == True:
+            return
+        if usbMountedb == True:
+            return
+        res = os.system("mount /dev/sdb1 /media/usb0 -o iocharset=utf8")
+        if res == 0:
+            usbMountedb = True
+    else:
+        if (usbMountedb == False):
+            return
+        os.system("umount /media/usb0")
+        usbMountedb = False
 
 if __name__ == '__main__':
     while True:
