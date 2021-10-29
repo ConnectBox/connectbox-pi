b2346015e84c9c0431e124d3b5da14c687190b54
Kirk Wilson <kirk.wilson@globalsystems.design>
2021-10-29 15:06:38 -0600

    Corrected mount/unmount of usb flash drives

--- ansible/roles/bootstrap/files/usr_local_connectbox_bin_mountCheck.py
+++ ansible/roles/bootstrap/files/usr_local_connectbox_bin_mountCheck.py
@@ -5,50 +5,113 @@
 
 import os
 import time
+import re
+
+DEBUG = 0
 
 global total
+total = 0
 c=["","",""]
-loc=[-1,-1,-1,-1,-1,-1,-1,-1,-1]
-mnt=[-1,-1,-1,-1,-1,-1,-1,-1,-1]
-d=["","","","","","","","","","",""]
+loc=[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+global mnt
+mnt=[-1,-1,-1,-1,-1,-1,-1,-1,-1, -1, -1]
+d=["","","","","","","","","","","","","","","","","","","","","",""]
 
 def mountCheck():
     global mnt
-    i=0
-    j=0
+    global total
+    j = 0                   #mount iterator looking for unmounted devices
+    b = os.popen('lsblk').read()
+    while (j < total):
+      if DEBUG: print("loop 1, iterate",j)
+      if (mnt[j] >= 0):
+        if not ('usb' + chr(ord('0')+j) in b):
+          c = "umount /media/usb" + chr(ord('0')+j)
+          res = os.system(c)
+          if DEBUG: print("completed unmount /media/usb", chr(ord('0')+j))
+          if not res:
+            mnt[j] = -1
+            if j>0:
+               os.popen('rmdir /media/usb'+chr(ord('0')+j))
+            j += 1
+          else:
+            if DEBUG: print("Failed to " + c)
+            mnt[j] = -1
+            j += 1
+        else:
+          #Were here with the device still mounted
+          j += 1
+      else:
+        #were here because there was no mount device detected which is odd as its supposed to be
+        if DEBUG: print("Error as device should have been mounted usb", j)
+        j += 1
+    i=0                       #line iterator
+    j=0                       #used for finding sdx1's
+    k=0                       #used for usb mounts
+    loc=[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
     b = os.popen('lsblk').read()
     c = b.partition("\n")
-    while ((c[2] != "") and (i<10)):
+# while we have lines to parse we check each line for an sdx1 device and mount it if not already
+    while ((c[0] != "") and (i<10)):
+      if DEBUG: print("Loop 2, iterate:",i, c[0])
       d[i] = c[0]
-      a = 'sd'+chr(ord("a")+j)+"1"
-      if a in d[i]:
+      e=re.search('sd[a-z]1', d[i])
+      if e: 
         loc[j]=i
-        j += 1 
+        if not (('usb' in d[i]) or ('part /' in d[i])):
+          while (k < 10) and (mnt[k] >= 0):
+            if DEBUG: print("loop 3, iterate:",k)
+            k += 1
+          if not (os.path.exists("/media/usb"+chr(ord("0")+k))):
+            res = os.system("mkdir /media/usb"+chr(ord("0")+k))
+          b = "mount /dev/" + e.group() + " /media/usb" + chr(ord('0')+k)
+          res = os.system(b)
+          if DEBUG: print("completed mount /dev/",e.group)
+          k += 1
+          j += 1
+        else:
+          if ('usb' in d[i]):
+            a = d[i].partition("usb")
+            if (a[2] != "") and (a[2].isalnum()):
+              l = ord(a[2])-ord("0")
+              mnt[l]=i
+              j+=1
+              if DEBUG: print("/dev/sdx1 is already mounted as usb",chr(l+ord('0')))
+            else:
+              if DEBUG: print("Error parsing usb# in line", i)
+              j+= 1
+          else:
+              if DEBUG: print("/dev/sdx1 is already mounted but not as usb", d[i])
+              j+= 1
       c = c[2].partition("\n")
       i += 1
-    d[i] = c[0]
-    a = 'sd'+chr(ord("a")+j)+"1"
-    if a in d[i]:
-      loc[j]=i
-      j += 1 
-    total = j
-    i = 0 
-    j = 0
-    while (i <= total):
-      if (mnt[i] < 0) and (loc[i] > 0):
-        c = d[i].partition("part")
-        if c[2] == " ":
-          a = 'sd'+chr(ord("a")+i)+"1"
-          b = "mount /dev/" + a + " /media/usb" + chr(j) + " -o iochrset=utf8"
-          res = os.system(b)
-          if res == 0:
-             mnt[i] = j
-             j += 1
-        else:
-          if (mnt[i] >= 0):
-            b = "umount /media/usb" + chr(mnt[i])
-            os.system(b)
-            mnt[i] = -1
+# now that we have looked at all lines in the current lsblk we need to count up the mounts
+    j = -1
+    i = 0
+    while (i < 10):            # we will check a maximum of 10 mounts
+      if DEBUG: print("loop 4, iterate:",i)
+      if (mnt[i] != -1):
+        i +=1
+        j +=1
+      else:                   # we have a hole or are done but need to validate
+        k = i+1
+        while (k < 10) and (mnt[i] == -1):  #checking next mount to see if it is valid
+          if DEBUG: print("loop 5, iterate:",k,i)
+          if (mnt[k] != -1):
+            mnt[i] = mnt[k]   # move the mount into this hole and clear the other point
+            j += 1
+            mnt[k] = -1
+            i += 1
+            k += 1
+            if DEBUG: print("had to move mount to new location",k," from ",i)
+          else:			# we have no mount here
+            k += 1
+        if (k == 10):           # if we have look at all mounts then were done otherwise we will check again
+          i=10
+    total = j+1
+    if DEBUG: print("total of devices mounted is", total)
+    if DEBUG: print("located", mnt)
+    return()
 
 
 if __name__ == '__main__':
@@ -56,4 +119,4 @@ if __name__ == '__main__':
         loop_time = 3
         mountCheck()
         time.sleep(loop_time)
-        
+
