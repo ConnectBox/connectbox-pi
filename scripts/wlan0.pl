efe668ea36b0b9d252283d582df776dd1bb08f33
Derek Maxson <dmaxson@frontporch.com>
2021-11-04 15:46:04 -0700

    WLAN0 support for Pi

---
+++ scripts/wlan0.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+# This program does steps necessary to unblock (rfkill) and init the wlan0 for client Wi-Fi on RPi
+
+my $rfkill = `sudo rfkill list 0 |grep 'Soft blocked: yes'`;
+
+print "rfkill says: $rfkill\n";
+
+if ($rfkill =~ /yes/) {
+	print "Unkilling the rfkill for wlan0\n";
+	system ("rfkill unblock 0");
+	system ("./wlan0.pl");
+	die;
+}
+else {
+	system ("ifdown wlan0");
+	system ("ifup wlan0");
+}
