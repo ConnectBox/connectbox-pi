f994bfc559bc34f3baf86531b623374c1df45fe7
Derek Maxson <dmaxson@frontporch.com>
2022-01-19 14:47:30 -0800

    Create folder structure in each language, plus some logging improvements

--- scripts/lazyLoader.py
+++ scripts/lazyLoader.py
@@ -18,7 +18,7 @@ print ("Handling File: " + url)
 os.system("wget '" + url + "' -O /tmp/openwell.zip")
 os.system("unzip -o /tmp/openwell.zip -d /var/www/enhanced/content/www/assets/")
 
-
+directories =  ["data", "images", "media", "web"]
 
 # Go through all the language folders in the package
 for language in next(os.walk(contentDirectory))[1]:
@@ -28,6 +28,17 @@ for language in next(os.walk(contentDirectory))[1]:
 		# Load main.json to process
 		f = open (contentDirectory + language + "/data/main.json")
 		thisMain = json.load(f)
+		for directory in directories:		
+			if os.path.isdir(contentDirectory + language + "/" + directory):
+				print ("	Directory Exists: " + contentDirectory + language + "/" + directory)
+			else:
+				try:
+					os.mkdir(contentDirectory + language + "/" + directory)
+					print ("	Created Directory at " + contentDirectory + language + "/" + directory)
+				except:
+					print ("	FAILED to Create Directory at " + contentDirectory + language + "/" + directory)
+		print (" ")
+		print ("=================================================================================")
 		for content in thisMain["content"]:
 			print (" Processing: " + content["title"])
 			f = open (contentDirectory + language + "/data/" + content["slug"] + ".json")
@@ -46,20 +57,25 @@ for language in next(os.walk(contentDirectory))[1]:
 				if (os.path.exists(contentDirectory + language + "/media/" + item["filename"])):
 					print ("	Content Exists: " + item["filename"])
 				else:
-					print ("	LOAD CONTENT: " + item["filename"])
+					print ("	LOAD CONTENT: " + item["filename"] + " from " + item["resourceUrl"])
 					try:
-						os.system("wget '" + item["resourceURL"] + "' -O " + contentDirectory + language + "/media/" + item["filename"])
+						os.system("wget '" + item["resourceUrl"] + "' -O " + contentDirectory + language + "/media/" + item["filename"])
 						print ("		Content Downloaded to: " + item["filename"])
 					except:
 						print ("		FAILED To Download: " + item["title"])
+				print ("----------------------------------------")
+				print (" ")
 				print ("	Checking Image Content: " + item["image"])
 				if (os.path.exists(contentDirectory + language + "/images/" + item["image"])):
 					print ("	Content Exists: " + item["image"])
 				else:
+					print ("	LOAD CONTENT: " + item["image"] + " from " + item["imageUrl"])
 					try:
-						os.system("wget '" + item["imageURL"] + "' -O " + contentDirectory + language + "/images/" + item["image"])
+						os.system("wget '" + item["imageUrl"] + "' -O " + contentDirectory + language + "/images/" + item["image"])
 						print ("		Content Downloaded to: " + item["image"])
 					except:
 						print ("		FAILED To Download: " + item["title"])
+				print ("----------------------------------------")
+				print (" ")
 
 print ("Done.")
