296c58d7566f20f63c45d09869a37ee354002b1a
Kirk Wilson <kirk.wilson@globalsystems.design>
2022-03-24 12:46:22 -0600

    added new scrpt to load wifi drivers if not already loaded for rc.local, also have commented out code for loading these alternate kernel drivers if they exist

---
+++ ansible/roles/network-interfaces/files/loadwifi.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+var1=$( modprobe -c | grep 8812au )
+var2=$( modprobe -c | grep 88x2bu )
+
+if [ ${#var1} -lt 5 ]							#Check if we have 8812au loaded
+then
+  echo "Loading 8812au"
+  insmod /usr/lib/$( uname -r)/kernel/net/wirless/8812au		#We found nothing so try to load this versions 8812au
+  modprobe 8812au
+  depmod -A
+else
+  echo "8812au module already loaded"
+fi
+
+if [ ${#var2} -lt 5 ]							#Check if we have 88x2bu loaded
+then
+  echo "Loading 88x2bu"
+  insmod /usr/lib/$( uname -r)/kernel/net/wirless/88x2bu		#We found nothing so try to load this versions 88x2bu
+  modprobe 88x2bu
+  depmod -A
+else
+  echo "88x2bu module already loaded"
+fi
+
+
