07cf27a262c68fc12379e3eba4badf4cd9866be2
Kirk Wilson <kirk.wilson@globalsystems.design>
2022-11-07 14:33:01 -0700

    Fixed ARM64 usage of 8812au compile.

--- ansible/roles/network-interfaces/files/8812au-install.sh
+++ ansible/roles/network-interfaces/files/8812au-install.sh
@@ -25,10 +25,10 @@ else
     sed -i 's/CONFIG_PLATFORM_ARM_RPI = n/CONFIG_PLATFORM_ARM_RPI = y/g' Makefile
     sed -i 's/CONFIG_POWER_SAVING = y/CONFIG_POWER_SAVING = n/g' Makefile
 		printf 'we check for 64 bit architecture $(uname -m)"
-	if [ ("aarch64" in ($uname -m)) ]
+	if ["aarch64" -eq $(uname -m)) ]
 	then
 	  ./ARM64_RPI.sh
-	  echo ("we ran ARM64_RPI.sh")
+	  echo "we ran ARM64_RPI.sh"
 	fi 
     if [ -f install.sh ];
     then
