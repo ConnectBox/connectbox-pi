3e106ced985f10b38d2b8c905fd6e6e602aad96b
Kirk Wilson <kirkdwilson@users.noreply.github.com>
2023-05-01 15:49:38 -0600

    Fixed errors in pathing

--- ansible/roles/network-interfaces/files/loadwifi.sh
+++ ansible/roles/network-interfaces/files/loadwifi.sh
@@ -7,7 +7,7 @@ var3=$( modprobe -c | grep 8852au )
 if [ ${#var1} -lt 5 ]							#Check if we have 8812au loaded
 then
   echo "Loading 8812au"
-  insmod /usr/lib/modules/$( uname -r)/kernel/net/drivers/wirless/8812au		#We found nothing so try to load this versions 8812au
+  insmod /usr/lib/modules/$( uname -r)/kernel/drivers/net/wireless/8812au		#We found nothing so try to load this versions 8812au
   modprobe 8812au
   depmod -A
 else
@@ -17,7 +17,7 @@ fi
 if [ ${#var2} -lt 5 ]							#Check if we have 88x2bu loaded
 then
   echo "Loading 88x2bu"
-  insmod /usr/lib/modules/$( uname -r)/kernel/net/drivers/wirless/88x2bu		#We found nothing so try to load this versions 88x2bu
+  insmod /usr/lib/modules/$( uname -r)/kernel/drivers/net/wireless/88x2bu		#We found nothing so try to load this versions 88x2bu
   modprobe 88x2bu
   depmod -A
 else
@@ -27,7 +27,7 @@ fi
 if [ ${#var3} -lt 5 ]							#Check if we have 88x2bu loaded
 then
   echo "Loading 8852au"
-  insmod /usr/lib/modules/$( uname -r)/kernel/drivers/net/wirless/8852au		#We found nothing so try to load this versions 88x2bu
+  insmod /usr/lib/modules/$( uname -r)/kernel/drivers/net/wireless/8852au		#We found nothing so try to load this versions 88x2bu
   modprobe 8852au
   depmod -A
 else
